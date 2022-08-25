878969db81c4347a73d56133915a85dad9460efd
Andrey Khodenkov <khodenkov2807@gmail.com>
2022-08-25 15:38:26 +0300

    Initial commit

---
+++ .prettier.js
@@ -0,0 +1,15 @@
+module.exports = {
+  trailingComma: 'es5',
+  tabWidth: 2,
+  singleQuote: true,
+  printWidth: 100,
+  endOfLine: 'auto',
+  overrides: [
+    {
+      files: '*.html',
+      options: {
+        printWidth: 100,
+      },
+    },
+  ],
+};
